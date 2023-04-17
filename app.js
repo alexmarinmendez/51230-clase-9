5c2aeeb1b650f1c0bce8bc6a7734e0baedf06551
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-17 09:25:56 -0500

    feat: add express.static

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import petsRouter from './routers/pets.router.js'
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true })) //solo es necesario cuando los datos se envian por formulario
+app.use(express.static('public')) // GET / (ruta raiz)
+// app.use('/contenido', express.static('public')) // GET /contenido (ruta raiz)
 
 app.use('/users', usersRouter)
 app.use('/pets', petsRouter)
