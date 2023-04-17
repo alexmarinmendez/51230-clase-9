3cdbacc147e2309446e2ec01c323b8ca4ca8af23
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-17 09:44:44 -0500

    feat: add middlewares

--- app.js
+++ app.js
@@ -2,31 +2,30 @@ import express from 'express' //ES Modules
 import usersRouter from './routers/users.router.js'
 import petsRouter from './routers/pets.router.js'
 
-const logged = true
+const logged = false
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) //solo es necesario cuando los datos se envian por formulario
 
-//middleware
-app.use(function (req, res, next) {
+const middleware1 = function (req, res, next) {
     console.log('Validando login de usuario que hizo la consulta...')
     if (logged) {
         next()
     } else {
         res.send({ error: 'No tienes permiso'})
     }
-})
+}
 
-app.use((req, res, next) => {
+const middleware2 = (req, res, next) => {
     console.log('Soy otro middleware')
     next()
-})
+}
 
 app.use(express.static('public')) // GET / (ruta raiz)
 // app.use('/contenido', express.static('public')) // GET /contenido (ruta raiz)
 app.use('/users', usersRouter)
-app.use('/pets', petsRouter)
+app.use('/pets', middleware2, middleware1,  petsRouter)
 
 
 app.listen(8080, () => console.log('Server Up'))
